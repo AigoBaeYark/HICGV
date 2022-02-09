9f73cc734a1611794b6fd34300ec2abb21242b1e
SeungjuSeo-github <seosj3104@naver.com>
2022-02-09 18:50:43 +0900

    MoviesDao 수정

--- src/main/java/com/hicgv/movies/dao/MoviesDao.java
+++ src/main/java/com/hicgv/movies/dao/MoviesDao.java
@@ -7,6 +7,6 @@ import com.hicgv.movies.dto.MoviesDto;
 public interface MoviesDao {
 	public ArrayList<MoviesDto> movies();
 	public MoviesDto moviesDetailview(String movie_id);
-	//public ArrayList<MovieTrailerDto> movieTrailer(String movie_id);
+	// public ArrayList<MovieTrailerDto> movieTrailer(String movie_id);
 	
 }
