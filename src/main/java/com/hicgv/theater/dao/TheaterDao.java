a547a6ab5ed00d351eb9d5759d1bd2cc6d62fa44
baaming <qogpal94@gmail.com>
2022-02-15 19:38:39 +0900

    TimeInfoDto 생성, TheaterDao.xml 추가, theater.js추가

--- src/main/java/com/hicgv/theater/dao/TheaterDao.java
+++ src/main/java/com/hicgv/theater/dao/TheaterDao.java
@@ -2,14 +2,14 @@ package com.hicgv.theater.dao;
 
 import java.util.ArrayList;
 
-import com.hicgv.movies.dto.MoviesDto;
 import com.hicgv.theater.dto.LocationDto;
 import com.hicgv.theater.dto.MoviesInfoDto;
-
+import com.hicgv.theater.dto.TimeInfoDto;
 
 public interface TheaterDao {
    public ArrayList<LocationDto> theater();
    public String getImg(String location_id);
-   public ArrayList<MoviesDto> getMoviesInfo(String movie_id);
-   public ArrayList<MoviesInfoDto> movietime(String movie_id);
+   public ArrayList<MoviesInfoDto> getMoviesInfo(String location_id);
+   public LocationDto getLocationInfo(String location_id);
+   public ArrayList<TimeInfoDto> getTime(String location_id);
 }
