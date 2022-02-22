63a394e4b2e9450378d80065f582e83f48bb0964
baaming <qogpal94@gmail.com>
2022-02-22 21:35:21 +0900

    TheaterController수정, TheaterDao.xml수정

--- src/main/java/com/hicgv/theater/dao/TheaterDao.java
+++ src/main/java/com/hicgv/theater/dao/TheaterDao.java
@@ -9,8 +9,9 @@ import com.hicgv.theater.dto.TimeInfoDto;
 public interface TheaterDao {
    public ArrayList<LocationDto> getTheaterInfo();
    public String getImg(String location_id);
-   public ArrayList<MoviesInfoDto> getMoviesInfo(String location_id);
+   public ArrayList<MoviesInfoDto> getMoviesInfo(String theater_id,String location_id,String date);
    public LocationDto getLocationInfo(String location_id);
    public ArrayList<TimeInfoDto> getTime(String location_id);
+
    public ArrayList<MoviesInfoDto> getAllMoviesInfo();
 }
